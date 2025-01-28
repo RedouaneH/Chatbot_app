c29863324481ba3b68daf585f14e4e7703b43d3c
Redouane <125410141+RedouaneH@users.noreply.github.com>
2025-01-28 18:15:40 +0100

    debuging

--- src/generation.py
+++ src/generation.py
@@ -8,14 +8,11 @@ def generate_web_search_prompt(db, query, top_k_url, chunk_size, overlap_ratio, 
     google_search_query = build_google_search_query(query, chat_history)
     print(f"\ngoogle_search_query : {google_search_query}\n")
     urls = search_google(google_search_query, top_k=top_k_url)
+    for u in urls:
+        print(u)
     db.load_from_urls(urls, chunk_size, int(overlap_ratio*chunk_size))
 
-    retriever = db.vector_store.as_retriever(
-        search_type="similarity_score_threshold",
-        search_kwargs={"k": top_k_similar, "score_threshold": score_threshold}
-    )
-
-    retrieved_chunks = [value.page_content for value in retriever.invoke(query)]
+    retrieved_chunks = db.retrieve(query, top_k_similar, score_threshold)
 
     web_search_prompt = get_web_search_prompt() + "\n\n".join(retrieved_chunks)
 
@@ -23,12 +20,7 @@ def generate_web_search_prompt(db, query, top_k_url, chunk_size, overlap_ratio, 
 
 def generate_rag_prompt(db, query, top_k_similar, score_threshold):
 
-    retriever = db.vector_store.as_retriever(
-        search_type="similarity_score_threshold",
-        search_kwargs={"k": top_k_similar, "score_threshold": score_threshold}
-    )
-
-    retrieved_chunks = [value.page_content for value in retriever.invoke(query)]
+    retrieved_chunks = db.retrieve(query, top_k_similar, score_threshold)
 
     rag_prompt = get_rag_prompt() + "\n\n".join(retrieved_chunks)
 
