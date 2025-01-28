eb5eae63cc23d38c804d130e63e93fd987b63452
Redouane <125410141+RedouaneH@users.noreply.github.com>
2025-01-28 18:23:15 +0100

    debugging

--- src/generation.py
+++ src/generation.py
@@ -1,14 +1,19 @@
 
 from data import *
 from db import *
+import sys
 
 
 def generate_web_search_prompt(db, query, top_k_url, chunk_size, overlap_ratio, top_k_similar, score_threshold, chat_history):
 
     google_search_query = build_google_search_query(query, chat_history)
     print(f"\ngoogle_search_query : {google_search_query}\n")
+    
     urls = search_google(google_search_query, top_k=top_k_url)
-    print("\ntest ---\n")
+    if len(urls)==0:
+        print("Urls est vide", file=sys.stderr)
+    for u in urls:
+        print(u, file=sys.stderr)
     db.load_from_urls(urls, chunk_size, int(overlap_ratio*chunk_size))
 
     retrieved_chunks = db.retrieve(query, top_k_similar, score_threshold)
